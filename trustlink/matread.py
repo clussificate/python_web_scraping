5e0014341ce81068f78f1caac562cf4f15a75f5f
clussificate <343260443@qq.com>
2017-09-15 16:05:18 +0800

    Delete matread.py

--- trustlink/matread.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-def read_excel(filename):
-    data = pd.read_excel(filename)
-    data2 = data.drop(['index'], axis=1)
-    adjarr = np.array(data2)
-    return adjarr
